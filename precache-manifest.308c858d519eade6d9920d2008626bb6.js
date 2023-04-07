42f8e659cb8efe46a34b49f76835636ea4a20ff4
Rafael Musa <rafa.musa@airbnb.com>
2023-04-06 21:04:44 -0700

    Updates

---
+++ precache-manifest.308c858d519eade6d9920d2008626bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df9fe1b3b30cf823902c",
+    "url": "/static/css/main.5f8a3cfc.chunk.css"
+  },
+  {
+    "revision": "df9fe1b3b30cf823902c",
+    "url": "/static/js/main.df9fe1b3.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "8e0fbeedba77eec4d35c",
+    "url": "/static/js/2.8e0fbeed.chunk.js"
+  },
+  {
+    "revision": "1458782b48af82eb3a96",
+    "url": "/static/js/runtime~main.1458782b.js"
+  },
+  {
+    "revision": "fd70ef84b17272120f5ebd772063cd7b",
+    "url": "/index.html"
+  }
+];

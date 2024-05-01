4cd714231b45296ae0739745a687dbd6abe05c74
awagner6 <37525104+awagner6@users.noreply.github.com>
2024-04-30 18:03:49 -0700

    Update Welcome.jsx

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -7,22 +7,19 @@ function Welcome() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // daylight savings
+    // Adjust for daylight savings or other temporal shifts
     const puzzleIndex = Math.floor((Date.now() - 1661131800000) / (1000 * 60 * 60 * 24));
     const haveSavedData = Number(localStorage.getItem('currentDay')) === puzzleIndex + 1;
     if (haveSavedData) {
-      navigate(`/play/${localStorage.getItem('currentMode') || "normal"}`)
+      navigate(`/play/${localStorage.getItem('currentMode') || "normal"}`);
     }
-  }, []);
+  }, [navigate]);
 
   return (
     <>
       <div className="welcome">Welcome to Anigrams!</div>
       <div className="welcomeSubtitle">A daily word game</div>
       <div className="welcomeEmojis">🦉🦒🐬🦜🐙🐐</div>
-      {/* <div className="welcomeBody">
-        <br />
-      </div> */}
       <div className="welcomeButtonWrapper">
         <div className="welcomeButtonContainer">
           <Link to="/play/normal" style={{ textDecoration: 'none' }}>
@@ -42,13 +39,20 @@ function Welcome() {
           </Link>
           <div>Solve without hints</div>
         </div>
-      </div>
-      <div className="welcomeButtonContainer noWelcomeMargin">
-        <button className="button-84 patreonButton" onClick={() => window.open('https://www.patreon.com/user?u=65757168', '_blank')}>Join Patreon</button>
-        <div className="welcomeAnigramsCopy">Access the archive of previous Anigrams puzzles, suggest new features, and play bonus puzzles</div>
+        <div className="welcomeButtonContainer">
+          <button className="button-84 patreonButton" onClick={() => window.open('https://www.patreon.com/user?u=65757168', '_blank')}>Join Patreon</button>
+          <div className="welcomeAnigramsCopy">Access the archive of previous Anigrams puzzles, suggest new features, and play bonus puzzles</div>
+        </div>
+        {/* New game announcement */}
+        <div className="welcomeButtonContainer">
+          <Link to="/orderup" style={{ textDecoration: 'none' }}>
+            <button className="button-84 welcomeButton">Order Up</button>
+          </Link>
+          <div>NEW! Adam's latest daily puzzle game has arrived! In Order Up, you have to arrange a list of items in order around a hidden theme. Try it at the link above!</div>
+        </div>
       </div>
     </>
-  )
-};
+  );
+}
 
 export default Welcome;
